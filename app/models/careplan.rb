5e6677a0ef9b5c08d723e309c35beaf7f301873f
imtaka <needer12345@gmail.com>
2020-05-06 19:15:26 +0900

    add validation in careplan model

--- app/models/careplan.rb
+++ app/models/careplan.rb
@@ -1,3 +1,13 @@
 class Careplan < ApplicationRecord
+
+  validates :create_plan, presence: true
+  validates :update_plan, presence: true
+  validates :care_task, length: { maximum: 500 }
+  validates :long_time_care_goal, presence: true, length: { in: 1..100 }
+  validates :short_time_care_goal, presence: true, length: { in: 1..100 }
+  validates :user_wants, length: { maximum: 100 }
+  validates :family_wants, length: { maximum: 100 }
+  validates :situation_achieved_goal, length: { maximum: 500 }
+
   belongs_to :customer
 end
