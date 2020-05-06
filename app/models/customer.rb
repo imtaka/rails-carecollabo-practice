81a33b518a53c571fcca70bab9e36559420775e7
imtaka <needer12345@gmail.com>
2020-05-06 19:07:16 +0900

    add validation in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
+    validates :name, presence: true, length: { in: 1..20 }
+    validates :gender, length: { in: 0..1 }
+    validates :age, length: { in: 40..120 }
+
     has_many :careplans
 end
